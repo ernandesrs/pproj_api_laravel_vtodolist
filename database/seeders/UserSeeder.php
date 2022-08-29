52d7c5d1a0ef2dc832d99f0a745ce2281ab5fecd
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-29 07:32:05 -0400

    User Seeder create

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create([
+            'email' => 'ernandesrsouza@gmail.com',
+            'first_name' => 'Ernandes',
+            'last_name' => 'Souza',
+            'password' => Hash::make("ernandes"),
+        ]);
+    }
+}
